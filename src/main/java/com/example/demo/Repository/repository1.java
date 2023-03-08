0331a35810fc258d0ac43f969fd0b185bff7cb54
V-HARI-PRASATH <113664554+V-HARI-PRASATH@users.noreply.github.com>
2023-03-08 15:00:47 +0530

    Delete src directory

--- src/main/java/com/example/demo/Repository/repository1.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Models.Items;
-
-public interface repository1 extends JpaRepository<Items,Integer> {
-
-}
